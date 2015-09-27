125a35dd579df9dd5ca9e7d1ed41234753635903
Khopa <clemguip@gmail.com>
2015-09-27 21:16:53 +0200

    PEP8 formatted

--- perceptron.py
+++ perceptron.py
@@ -20,6 +20,7 @@ Options:
 Try to determine if a point is upper above a curve without know this curve :)
 """
 
+
 class Perceptron(object):
     """
     The simplest neural net possible
@@ -27,13 +28,14 @@ class Perceptron(object):
     And return a result following the sign of the of sum
     multiply by each weight input
     """
+
     def __init__(self, n=2):
         """
         Constructor initializes perceptron
         n = number of inputs excluding bias
         """
         # At first ways weights are initialized to random values
-        self.weights = [ round(random.uniform(-1.0,1.0),3) for weight in range(n+1) ]
+        self.weights = [round(random.uniform(-1.0, 1.0), 3) for weight in range(n + 1)]
         # Arbitrary chosen
         self.learning_control = 0.01
 
@@ -45,7 +47,7 @@ class Perceptron(object):
         """
         processed_inputs = inputs[:]
         processed_inputs.append(1)
-        inputs_sum = sum([ input_value*self.weights[i] for i,input_value in enumerate(processed_inputs)])
+        inputs_sum = sum([input_value * self.weights[i] for i, input_value in enumerate(processed_inputs)])
         return (1, processed_inputs) if inputs_sum > 0 else (-1, processed_inputs)
 
     def train(self, inputs, desired):
@@ -58,7 +60,7 @@ class Perceptron(object):
         guess, processed_inputs = self.feeding(inputs)
         error = desired - guess
         for i, weight in enumerate(self.weights):
-            self.weights[i] += self.learning_control*error*processed_inputs[i];
+            self.weights[i] += self.learning_control * error * processed_inputs[i];
 
     def exam(self, inputs, desired):
         """
@@ -71,10 +73,12 @@ class Perceptron(object):
         error = desired - guess
         return (inputs, 0, guess) if error != 0 else (inputs, 1, guess)
 
+
 class World:
     """
     Create a 2D environment to visualize Perceptron behaviors
     """
+
     def __init__(self, nb_points=10, dim=(800, 600), slower=0.1, training_function="100*sin(0.01*x)"):
         self.nb_points = int(nb_points)
         self.dim = dim
@@ -98,41 +102,41 @@ class World:
         self.points.append(result)
 
     def shifting_point(self, point):
-        return [point[0]+self.dim[0]/2, -(point[1]-self.dim[1]/2)]
+        return [point[0] + self.dim[0] / 2, -(point[1] - self.dim[1] / 2)]
 
     def slow_down(self, delta=0.1):
         """
         Append a point to the dispaying each delta second
         """
-        #No more point can be displayed
+        # No more point can be displayed
         if self.previous_index == len(self.points):
             return
 
-        current_time =  time.time()
+        current_time = time.time()
         currentDelta = current_time - self.previous_time
 
         if currentDelta > delta:
             self.previous_time = current_time
             self.displayed_points.append(self.points[self.previous_index])
-            self.previous_index+=1
+            self.previous_index += 1
 
     def check_accurency(self):
-        return round(100*sum([point[1] for point in self.displayed_points])/float(len(self.displayed_points)), 2)
+        return round(100 * sum([point[1] for point in self.displayed_points]) / float(len(self.displayed_points)), 2)
 
     def final_accurency(self):
-        return round(100*sum([point[1] for point in self.points])/float(len(self.points)), 2)
+        return round(100 * sum([point[1] for point in self.points]) / float(len(self.points)), 2)
 
     def run(self):
         """
         Run the world
         """
-        BLACK = (0,0,0)
-        WHITE = (255,255,255)
-        BLUE  = (0,0,255)
-        RED  = (255,0,0)
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+        BLUE = (0, 0, 255)
+        RED = (255, 0, 0)
         ORANGE = (237, 195, 49)
-        GREEN  = (0,255,0)
-        GREY   = (212, 210, 210)
+        GREEN = (0, 255, 0)
+        GREY = (212, 210, 210)
         font = pygame.font.Font(None, 36)
 
         final_accurency = self.final_accurency()
@@ -148,10 +152,11 @@ class World:
                 if event.type == pygame.QUIT:
                     run = False
 
-            #draw line
+            # draw line
             w, h = self.shifting_point(self.dim)
-            for x in range(-w/2, w/2):
-                pygame.draw.circle(self.screen, BLACK, self.shifting_point([int(x), int(self.training_function(x=x))]), 1)
+            for x in range(-w / 2, w / 2):
+                pygame.draw.circle(self.screen, BLACK, self.shifting_point([int(x), int(self.training_function(x=x))]),
+                                   1)
 
             #slow down animation
             self.slow_down(self.slower)
@@ -171,10 +176,10 @@ class World:
             frame = pygame.Surface((300, 100))
             frame.set_alpha(200)
             frame.fill(GREY)
-            self.screen.blit(frame, (0,0))
+            self.screen.blit(frame, (0, 0))
             self.screen.blit(final_accurency_text, (20, 20))
             self.screen.blit(accurency_text, (20, 60))
-            if(len(self.displayed_points) == len(self.points)):
+            if (len(self.displayed_points) == len(self.points)):
                 self.screen.blit(end, (20, 100))
             pygame.display.flip()
 
@@ -185,10 +190,12 @@ class World:
         """
         points = []
         for point in range(self.nb_points):
-            point = [random.randrange(-self.dim[0]/2, self.dim[0]/2), random.randrange(-self.dim[1]/2, self.dim[1]/2)]
+            point = [random.randrange(-self.dim[0] / 2, self.dim[0] / 2),
+                     random.randrange(-self.dim[1] / 2, self.dim[1] / 2)]
             good = 1 if self.training_function(x=point[0]) > point[1] else -1
             points.append((point, good))
         return points
+
 
 if __name__ == "__main__":
     arguments = docopt(help)
@@ -197,7 +204,7 @@ if __name__ == "__main__":
                   training_function=arguments["--training_function"])
     training_values = world.generate_world()
     for point in training_values:
-        for training in range(int(arguments["--nb_trainings"])-1):
+        for training in range(int(arguments["--nb_trainings"]) - 1):
             p.train(*point)
         result = p.exam(*point)
         world.add_result(result)
