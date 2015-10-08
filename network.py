cee100d538fe6a4bb499434db2951583a27a6500
Yannick Guern <yannick.guern.bzh@gmail.com>
2015-10-09 00:49:40 +0200

    Use matricial form to represent neural network

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import re
-from neuron import Sigmoid
+import random
+import numpy as np
 from pprint import pprint
 from docopt import docopt
 
@@ -20,7 +21,7 @@ class LayeredNetwork(object):
     """
     Make a network where each neurons of one layer are linked to each neurons of previous layer
     """
-    def __init__(self, layout):
+    def __init__(self, layout, eta=0.001):
         """
         A layout parameter is a string describing network architecture
         the pattern is for example 784_12_15_10, that create a layered network
@@ -28,68 +29,104 @@ class LayeredNetwork(object):
         and 15 neurons and returns its results with a pool of 10 outputs. Of course
         hidden layers aren't mandatory so you can write 784_10 if you want and you
         will get 784 inputs linked to 10 outputs.
+        eta handles network learning creativity
         """
         pattern = re.compile("([\d_]*)")
         matched = re.findall(pattern, layout)
         if not matched:
             raise ValueError("Incorrect layout provided")
-        nb_neurons_on_layers = matched[0].split("_")
-        self.layers = [int(nb_neuron_on_this_layer) for nb_neuron_on_this_layer in nb_neurons_on_layers][::-1]
-        self.neurons = {}
-        self.__generate()
+        sizes = [ int(x) for x in matched[0].split("_") ]
+        self.eta = eta
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [ np.random.randn(y, 1) for y in sizes[1:] ]
+        self.weights = [ np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:]) ]
 
-    def __generate(self):
+    def schotastic_gradient_descent(self, batch, nb_trainings, batch_size):
         """
-        Create the network
+        Following a specific algorithm trains the network to handle various
+        situation, batches is a list of (x, y), where x is the input vector
+        and y is the output vector. nb_trainings specifies how many train will
+        be performed and batch_size
         """
-        neuron_id = 0
-        layer_id = len(self.layers)
-        neurons_on_next_layer = []
-        layers = {}
+        print "Starting training"
+        n = len(batches)
+        for training_num in range(nb_trainings):
+            random.shuffle(batches)
+            training_data = [ batch[i:i+batch_size] for i in xrange(0, n batch_size) ]
+            for batch in training_data:
+                self.training(batch)
+        print "End of training"
 
-        #create layers and links between them
-        for layer in self.layers:
-            neurons_on_next_layer_tmp = []
-            layers[layer_id] = []
-            for i in range(layer):
-                neuron = Sigmoid(nb_inputs=1)
-                neurons_on_next_layer_tmp.append((neuron, neuron_id))
-                self.neurons[neuron_id] = {
-                    "neuron": neuron,
-                    "linked_to" : neurons_on_next_layer,
-                    "layer" : layer_id,
-                }
-                layers[layer_id].append(neuron)
-                neuron_id += 1
-            neurons_on_next_layer = neurons_on_next_layer_tmp[:]
-            layer_id -= 1
-
-        #set correct inputs number for each neuron
-        number_on_previous_layer = 1 #set to 1 because if we're at first layer each neuron has 1 input
-        for layer in sorted(layers.keys()):
-            for neuron in layers[layer]:
-                neuron.setNbInputs(number_on_previous_layer)
-            number_on_previous_layer = len(layers[layer])
-
-    def draw_network(self):
+    def feedforward(self, a):
         """
-        Create a DOT file showing network architecture
+        Return the output of the network if "a" is input
         """
-        with open("network.dot", "w") as fd:
-            fd.write("digraph {\n")
-            for neuron_id in self.neurons.keys():
-                if self.neurons[neuron_id]["layer"] == 1:
-                    fd.write("%s [color=blue];" % (neuron_id,))
-                elif self.neurons[neuron_id]["layer"] == len(self.layers):
-                    fd.write("%s [color=red];" % (neuron_id,))
-                for linked_neuron in self.neurons[neuron_id]["linked_to"]:
-                    fd.write("%s -> %s;\n" % (neuron_id, linked_neuron[1]))
-                if not self.neurons[neuron_id]["linked_to"]:
-                    fd.write("%s;\n" % (neuron_id))
-            fd.write("}")
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a # output vecor of the last layer
 
+    def training(self, batch):
+        #initiate nabla list
+        nabla_b = [ np.zeros(b.shape) for b in self.biases ]
+        nabla_w = [ np.zeros(w.shape) for w in self.weights ]
+        # launch different training inputs x and compute results regarding expected value y
+        for x, y in batch:
+            delta_nabla_b, delta_nabla_w = self.backpropagation(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_b = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        #modify network biases and weights following network outputs
+        self.weights = [w - (self.eta/len(batch)) * nw for w,nw in zip(self.weights, nabla_w)]
+        self.weights = [b - (self.eta/len(batch)) * nb for w,nb in zip(self.biases, nabla_b)]
+
+    def backpropagation(self, x, y):
+        """
+        x input vector
+        y expected output vector
+        return delta_nabla_b and delta_nabla_w, which are lists of optimized delta to append
+        to current network biases and weight to reach with the fastest way the expected result
+        """
+        #initiate nabla list
+        delta_nabla_b = [ np.zeros(b.shape) for b in self.biases ]
+        delta_nabla_w = [ np.zeros(w.shape) for w in self.weights ]
+        #initiate feed forwarding
+        a = x
+        activations = [x]
+        zs = []
+        #propagate inputs toward outputs
+        for b,w in zip(self.biases, self.weights):
+            z = np.dot(w, a) + b
+            zs.append(z)
+            a = sigmoid(z)
+            activations.append(a)
+        #output checking
+        delta = (activations[-1] - y), sigmoid_prime(zs[-1])
+        #backpropagation initialization
+        delta_nabla_b[-1] = delta
+        delta_nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        #backpropagation
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sigmoid_prime(z)
+            delta_nabla_b[-l] = delta
+            delta_nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
+        return (delta_nabla_b, delta_nabla_w)
+
+def sigmoid(z):
+    """
+    An elementwise activation function
+    z a vector
+    """
+    return 1.0 / (1.0 + np.exp(-z))
+
+def sigmoid_prime(z):
+    """
+    An elementwise prime activation function
+    z a vector
+    """
+    return sigmoid(z) * (1.0 - sigmoid(z))
 
 if __name__ == '__main__':
     arguments = docopt(help)
     network = LayeredNetwork(layout=arguments["--layout"])
-    network.draw_network()
